379776144c16165d36406e484fe82d433367c398
Kira <kirazy@live.com>
2026-06-07 03:57:31 -0700

    refactor: split refugiums, they have nothing in common

---
+++ graphics-packs/angels/bioprocessing/refugium-biter-graphics-pack.lua
@@ -0,0 +1,96 @@
+local _defines = require("api.defines")
+
+local CraftingMachineGraphicsPack = require("graphics-packs.abstractions.crafting-machine-graphics-pack")
+
+---@class Reskins.Angels.RefugiumBiterGraphicsPack:Reskins.Abstractions.CraftingMachineGraphicsPack
+local RefugiumBiterGraphicsPack = {}
+RefugiumBiterGraphicsPack.__index = CraftingMachineGraphicsPack
+
+-- Set up inheritance
+setmetatable(RefugiumBiterGraphicsPack, {
+	__index = CraftingMachineGraphicsPack,
+})
+
+---@class Reskins.Angels.RefugiumBiterGraphicsPackParams
+---@field tint data.Color?
+
+---@param params Reskins.Angels.RefugiumBiterGraphicsPackParams
+---@return Reskins.Angels.RefugiumBiterGraphicsPack
+---@nodiscard
+function RefugiumBiterGraphicsPack:configure(params)
+	local instance = CraftingMachineGraphicsPack.configure(self, {
+		tint = params.tint,
+		required_assets = {
+			[_defines.assets.bioprocessing_graphics] = true,
+		},
+		nominal_width = 7,
+		nominal_height = 7,
+		graphics_set = self.get_graphics_set(params.tint),
+	}) --[[@as Reskins.Angels.RefugiumBiterGraphicsPack]]
+
+	-- Set the correct metatable for this class.
+	setmetatable(instance, RefugiumBiterGraphicsPack)
+	return instance
+end
+
+---@param tint data.Color?
+---@return data.CraftingMachineGraphicsSet
+---@nodiscard
+function RefugiumBiterGraphicsPack.get_graphics_set(tint)
+	local working_visualisations = {
+		{
+			animation = {
+				filename = "__angelsbioprocessinggraphics__/graphics/entity/bio-refugium/bio-refugium-biter.png",
+				width = 288,
+				height = 288,
+				line_length = 4,
+				frame_count = 16,
+				shift = { 0, 0 },
+				animation_speed = 0.5 * 0.75 / 2,
+			},
+		},
+	}
+
+	if tint then
+		table.insert(working_visualisations, {
+			always_draw = true,
+			animation = {
+				layers = {
+					-- Mask
+					{
+						filename = "__reskins-assets-angels__/graphics/entity/refugium-biter/refugium-biter-mask.png",
+						priority = "extra-high",
+						width = 288,
+						height = 288,
+						shift = { 0, 0 },
+						tint = tint,
+					},
+					-- Highlights
+					{
+						filename = "__reskins-assets-angels__/graphics/entity/refugium-biter/refugium-biter-highlights.png",
+						priority = "extra-high",
+						width = 288,
+						height = 288,
+						shift = { 0, 0 },
+						blend_mode = "additive-soft",
+					},
+				},
+			},
+		})
+	end
+
+	---@type data.CraftingMachineGraphicsSet
+	return {
+		animation = {
+			filename = "__angelsbioprocessinggraphics__/graphics/entity/bio-refugium/bio-refugium-biter-off.png",
+			width = 288,
+			height = 288,
+			line_length = 1,
+			frame_count = 1,
+			shift = { 0, 0 },
+		},
+		working_visualisations = working_visualisations,
+	}
+end
+
+return RefugiumBiterGraphicsPack
---
+++ graphics-packs/angels/bioprocessing/refugium-fish-graphics-pack.lua
@@ -0,0 +1,108 @@
+local _defines = require("api.defines")
+
+local CraftingMachineGraphicsPack = require("graphics-packs.abstractions.crafting-machine-graphics-pack")
+
+---@class Reskins.Angels.RefugiumFishGraphicsPack:Reskins.Abstractions.CraftingMachineGraphicsPack
+local RefugiumFishGraphicsPack = {}
+RefugiumFishGraphicsPack.__index = CraftingMachineGraphicsPack
+
+-- Set up inheritance
+setmetatable(RefugiumFishGraphicsPack, {
+	__index = CraftingMachineGraphicsPack,
+})
+
+---@class Reskins.Angels.RefugiumFishGraphicsPackParams
+---@field tint data.Color?
+
+---@param params Reskins.Angels.RefugiumFishGraphicsPackParams
+---@return Reskins.Angels.RefugiumFishGraphicsPack
+---@nodiscard
+function RefugiumFishGraphicsPack:configure(params)
+	local instance = CraftingMachineGraphicsPack.configure(self, {
+		tint = params.tint,
+		required_assets = {
+			[_defines.assets.bioprocessing_graphics] = true,
+		},
+		nominal_width = 7,
+		nominal_height = 7,
+		graphics_set = self.get_graphics_set(params.tint),
+	}) --[[@as Reskins.Angels.RefugiumFishGraphicsPack]]
+
+	-- Set the correct metatable for this class.
+	setmetatable(instance, RefugiumFishGraphicsPack)
+	return instance
+end
+
+---@param tint data.Color?
+---@return data.CraftingMachineGraphicsSet
+---@nodiscard
+function RefugiumFishGraphicsPack.get_graphics_set(tint, variant)
+	local working_visualisations = {
+		{
+			animation = {
+				filename = "__angelsbioprocessinggraphics__/graphics/entity/bio-refugium/bio-refugium-fish.png",
+				width = 288,
+				height = 288,
+				line_length = 7,
+				frame_count = 49,
+				shift = { 0, 0 },
+				animation_speed = 49 / 90,
+			},
+		},
+	}
+
+	if tint then
+		table.insert(working_visualisations, {
+			always_draw = true,
+			animation = {
+				layers = {
+					-- Mask
+					{
+						filename = "__reskins-assets-angels__/graphics/entity/refugium-fish/refugium-fish-mask.png",
+						priority = "extra-high",
+						width = 288,
+						height = 288,
+						shift = { 0, 0 },
+						tint = tint,
+					},
+					-- Highlights
+					{
+						filename = "__reskins-assets-angels__/graphics/entity/refugium-fish/refugium-fish-highlights.png",
+						priority = "extra-high",
+						width = 288,
+						height = 288,
+						shift = { 0, 0 },
+						blend_mode = "additive-soft",
+					},
+				},
+			},
+		})
+	end
+
+	---@type data.CraftingMachineGraphicsSet
+	return {
+		animation = {
+			layers = {
+				{
+					filename = "__angelsbioprocessinggraphics__/graphics/entity/bio-refugium/bio-refugium-fish-shadow.png",
+					width = 288,
+					height = 288,
+					line_length = 1,
+					frame_count = 1,
+					shift = { 0, 0 },
+				},
+				{
+					filename = "__angelsbioprocessinggraphics__/graphics/entity/bio-refugium/bio-refugium-fish-off.png",
+					width = 288,
+					height = 288,
+					line_length = 1,
+					frame_count = 1,
+					shift = { 0, 0 },
+				},
+			},
+		},
+		working_visualisations = working_visualisations,
+	}
+end
+
+return RefugiumFishGraphicsPack
